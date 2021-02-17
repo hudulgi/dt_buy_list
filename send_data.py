6defef93603ca7175b749c95dfc0afe961244c56
hudulgi <jeonghu@gmail.com>
2021-02-18 01:47:05 +0900

    Initial commit

---
+++ send_data.py
@@ -0,0 +1,40 @@
+import pymysql
+from DB_info import *
+
+
+class MySQL:
+    def __init__(self, _info):
+        '''
+        sql_eng = "mysql+mysqldb://%s:%s@%s:%s/%s" % (DBInfo['user'], DBInfo['password'], DBInfo['host'],
+                                                      DBInfo['port'], DBInfo['db'])
+        self.engine = create_engine(sql_eng, encoding=DBInfo['charset'])
+        '''
+        self.conn = pymysql.connect(host=_info['host'],
+                                    port=_info['port'],
+                                    user=_info['user'],
+                                    password=_info['password'],
+                                    db=_info['db'],
+                                    charset=_info['charset'])
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def create_table(self, _table_name, _line):
+        sql = f"SHOW TABLES LIKE '{_table_name}'"
+        result = self.cursor.execute(sql)
+        if not result:
+            sql = f"create table {_table_name}{_line};"
+
+            self.cursor.execute(sql)
+            self.conn.commit()
+        else:
+            print("테이블 존재")
+
+
+DB_info['db'] = 'dt_king'
+mydb = MySQL(DB_info)
+mydb.create_table("buy_list", "(Date datetime primary key,Code char(7),OBJ Decimal,Price Decimal)")
+mydb.close()
+
+#Date date primary key,Open Decimal,High Decimal,Low Decimal,Close Decimal, Volume Decimal
